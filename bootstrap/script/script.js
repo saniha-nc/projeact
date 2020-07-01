307b626cdea2a7dab3d1344c1ec13e85f42bfa8f
Saniha N C <sanihanc@gmail.com>
2020-07-01 17:40:33 +0530

    bootstrap webpage

---
+++ bootstrap/script/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+	$('#hide').click(function(){
+		$('#sente').hide();
+	});
+	$('#show').click(function(){
+		$('#sente').show();
+	});
+	$('#csshide').click(function(){
+		$('#csssen').css('display','none');
+
+	});
+	$('#cssshow').click(function(){
+		$('#csssen').css('display','block');
+
+	});
+
+});
+
